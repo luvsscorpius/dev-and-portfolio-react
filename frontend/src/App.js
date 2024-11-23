bc930d60024d6701ac217d9118afdd200df0a6a3
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2024-11-22 21:12:25 -0300

    feat(header): creating header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Header } from './Components/Header/Header';
 
 function App() {
   return (
     <>
+    <Header/>
     </>
   );
 }
---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as H from './Styles'
+
+export const Header = () => {
+  return (
+    <H.HeaderContainer>
+
+    </H.HeaderContainer>
+  )
+}
