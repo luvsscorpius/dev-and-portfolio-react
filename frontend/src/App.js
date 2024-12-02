be8369a97c680a694f30dcf581c639905b366387
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2024-12-02 15:41:19 -0300

    feat(servicos): starting servicos page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Header } from './Components/Header/Header';
 import { GlobalStyle } from './Css/GlobalStyle';
 import { Home } from './Pages/Home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import { Servicos } from './Pages/Servicos/Servicos';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/servicos' element={<Servicos />}/>
         </Routes>
         <GlobalStyle />
       </Router>
