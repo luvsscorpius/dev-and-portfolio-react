495010831ce4d2c6b80b80d5c3095c65a6ce59ea
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2024-12-02 15:39:18 -0300

    feat(react router): implementing routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { Header } from './Components/Header/Header';
 import { GlobalStyle } from './Css/GlobalStyle';
 import { Home } from './Pages/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
 
 function App() {
   return (
     <>
-    <Header/>
-    <Home/>
-    <GlobalStyle/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+        <GlobalStyle />
+      </Router>
     </>
   );
 }
