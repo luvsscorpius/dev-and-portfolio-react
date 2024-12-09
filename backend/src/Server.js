f97f4b61b91f1e706703652c76ac2424c1d5bdef
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2024-12-09 18:34:14 -0300

    feat(contactpage): creating sendEmail route on backend folder

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -3,8 +3,10 @@ const app = express()
 const port = 2000
 
 const indexRoute = require('./Routes/indexRoute')
+const sendEmailRoute = require('./Routes/sendEmail')
 
 app.use('/', indexRoute)
+app.use('/sendEmail', sendEmailRoute)
 
 app.listen(port, () => {
     console.log("Servidor rodando na porta ", port)
