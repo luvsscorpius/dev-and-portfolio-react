332f96b805bb169aceb2f61a81450fc04bf07932
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2024-12-14 13:40:31 -0300

    fix(backend): installing cors again

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -3,17 +3,7 @@ const app = express()
 const port = 2000
 const cors = require('cors')
 require('dotenv').config();
-
-const corsOptions = {
-    origin: ['https://luvsscorpius.github.io'], // Domínio permitido
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization', "Access-Control-Allow-Methods", "Access-Control-Request-Headers"], // Cabeçalhos permitidos
-    credentials: true,
-    enablePreflight: true
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions))
+app.use(cors())
 
 // Middleware para processar json nas requisicoes
 app.use(express.json())
