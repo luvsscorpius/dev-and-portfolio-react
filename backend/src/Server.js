0c0b59e2e45fbd9245df35e30d5a6983131e8b8b
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2024-12-14 13:18:00 -0300

    feat(backend): using cors to allow access

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -3,7 +3,12 @@ const app = express()
 const port = 2000
 const cors = require('cors')
 require('dotenv').config();
-app.use(cors())
+
+app.use(cors({
+    origin: 'https://luvsscorpius.github.io/dev-and-portfolio-react/', // Domínio permitido
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
+  }));
 
 // Middleware para processar json nas requisicoes
 app.use(express.json())
