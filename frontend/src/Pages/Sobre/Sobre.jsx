58d4a3e5b975df4907c675a163e0f4a6634d421b
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2025-06-21 17:58:37 -0300

    feat(sobre.jsx): creating a time line

--- frontend/src/Pages/Sobre/Sobre.jsx
+++ frontend/src/Pages/Sobre/Sobre.jsx
@@ -4,7 +4,6 @@ import * as S from './Styles'
 export const Sobre = () => {
 
   return (
-    <main>
         <S.sobreMimContainer>
             <S.titleContainer>
                 <h2>Sobre mim</h2>
@@ -15,7 +14,58 @@ export const Sobre = () => {
                 Olá, sou Anderson, mas pode me chamar de And! Sou desenvolvedor fullstack, apaixonado por transformar ideias em soluções completas e funcionais. Ao longo da minha jornada, tenho aprimorado minhas habilidades tanto no front-end quanto no back-end, sempre focado em entregar códigos eficientes e uma excelente experiência para os usuários. Amo tecnologia e estou sempre pronto para compartilhar o que aprendo, enquanto me inspiro com uma boa música para impulsionar minha criatividade e motivação.
                 </p>
             </S.descriptionContainer>
+
+            <S.linhaDoTempoContainer>
+                <S.Linha/>
+
+                <S.OutSpan>
+                    <p>
+                      Início do Curso Técnico em Informática
+                    </p>
+
+                    <span>
+                      <p>
+                        Out, 2021
+                      </p>
+                    </span>
+                </S.OutSpan>
+
+                <S.OutSpan>
+                    <p>
+                      Estágio na empresa OK Informática
+                    </p>
+
+                    <span>
+                      <p>
+                        Nov, 2022
+                      </p>
+                    </span>
+                </S.OutSpan>
+
+                <S.OutSpan>
+                    <p>
+                      Conclusão do Curso Técnico em Informática/Estagio
+                    </p>
+
+                    <span>
+                      <p>
+                        Mar/Abr, 2023
+                      </p>
+                    </span>
+                </S.OutSpan>
+      
+                <S.OutSpan>
+                    <p>
+                      Desenvolvimento de Projetos Pessoais/Freelances
+                    </p>
+
+                    <span>
+                      <p>
+                        2024/2025
+                      </p>
+                    </span>
+                </S.OutSpan>
+            </S.linhaDoTempoContainer>
         </S.sobreMimContainer>
-    </main>
   )
 }
--- frontend/src/Pages/Sobre/Styles.jsx
+++ frontend/src/Pages/Sobre/Styles.jsx
@@ -41,6 +41,65 @@ export const descriptionContainer = styled.div`
 `
 
 export const linhaDoTempoContainer = styled.div`
+    position: relative;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 50px;
+    overflow-y: hidden;
+    overflow-x: auto;
 
+    @media (min-width: 1024px) {
+        justify-content: center;
+    }
+`
+
+export const OutSpan = styled.div`
+    position: relative;
+    z-index: 1;
+    width: 220px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 10px;
+    padding: 0 20px 0 20px;
+    height: 150px;
+
+    p {
+        color: #eee;
+        font-family: "Poppins", serif;
+        font-weight: 600;
+        text-align: center;
+    }
+
+    span {
+        p {
+            background-color: #382AB6;
+            border-radius: 100px;
+            width: 150px;
+            text-align: center;
+        }
+    }
+
+    @media (min-width: 1024px) {
+        width: 200px;
+    }
+`
+export const Linha = styled.div`
+    position: absolute;
+    top: 78%;
+    left: 30px;
+    width: 800px;
+    height: 4px;
+    z-index: 0;
+    background-color: #eee;
+
+    @media (min-width: 1024px) {
+        left: 50%;
+        transform: translateX(-50%);
+    }
 `
 
